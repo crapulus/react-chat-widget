9a17eb3f1555ed8f8a652cc0dacc0fbd0990e58a
VAN BELLE Marc <GCA590@n-allo.intra>
2017-07-07 13:59:05 +0200

    restored SW + modifs html samples

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+// // Check for browser support of service worker
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('service-worker.js')
+//   .then(function(registration) {
+//     // Successful registration
+//     console.log('Service worker Registration successful, scope is:', registration.scope);
+//   }).catch(function(err) {
+//     // Failed registration, service worker won’t be installed
+//     console.log('Whoops. Service worker registration failed, error:', err);
+//   });
+// }
 
 ReactDOM.render(<App/>, document.getElementById('nrcwv1'));
-
-//const app = document.createElement("div",{"id":"app"});
-//ReactDOM.render(<Widget/>, app);
-// eslint-disable-next-line
+registerServiceWorker();
 
 if (module.hot) {
   module.hot.accept('./App', () => {
